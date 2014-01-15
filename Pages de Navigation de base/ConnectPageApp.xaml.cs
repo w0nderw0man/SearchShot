a28d85d382af6c7556b4963cd19e1fdfe6c18557
johnnybgoodemu <jonathan.bastier@gmail.com>
2014-01-15 10:42:34 +0100

    Navigation

---
+++ Pages de Navigation de base/ConnectPageApp.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace SearchShot
+{
+    public partial class ConnectPageApp : PhoneApplicationPage
+    {
+        public ConnectPageApp()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectionMode.ConnectWith = "App";
+            ConnectionMode.Name = "Test";
+            ConnectionMode.Token = "TEST1";
+            NavigationService.Navigate(new Uri("/Accueil.xaml", UriKind.Relative));
+        }
+    }
+}
