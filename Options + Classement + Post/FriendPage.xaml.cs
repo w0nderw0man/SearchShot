9a3ccd84f4d4fb1335e652c92c7af5faa659d9ea
johnnybgoodemu <jonathan.bastier@gmail.com>
2014-01-15 10:43:19 +0100

    Page Option

---
+++ Options + Classement + Post/FriendPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace SearchShot
+{
+    public partial class FriendPage : PhoneApplicationPage
+    {
+        public FriendPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Pivot_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ Options + Classement + Post/OptionsMenu.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace SearchShot
+{
+    public partial class OptionsMenu : PhoneApplicationPage
+    {
+        public OptionsMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAvatar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnInfos_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
